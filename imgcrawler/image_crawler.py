7b3c4ee0b2f4abeb445700e7094523d5533cc56f
nomnomnonono <murumuru1204@gmail.com>
2021-08-23 17:29:28 +0900

    update file structure to use ImageDataGenerator.

--- imgcrawler/image_crawler.py
+++ imgcrawler/image_crawler.py
@@ -11,8 +11,11 @@ class ImageCrawler:
     def crawl(self, num):
         self.num = num
         for name in self.names:
-            os.mkdir(name)
-            crawler = GoogleImageCrawler(storage={"root_dir": name})
+            if os.path.exists(name[1]):
+                shutil.rmtree(name[1])
+
+            os.mkdir(name[1])
+            crawler = GoogleImageCrawler(storage={"root_dir": name[1]})
             crawler.crawl(keyword=name, max_num=self.num)
 
     def split(self, val_rate):
@@ -22,15 +25,26 @@ class ImageCrawler:
         if not os.path.exists("validation"):
             os.mkdir("validation")
 
+        for i in range(3):
+            if not os.path.exists(f"train/{str(i)}"):
+                os.mkdir(f"train/{str(i)}")
+
+            if not os.path.exists(f"validation/{str(i)}"):
+                os.mkdir(f"validation/{str(i)}")
+
         for name in self.names:
-            files = glob.glob(os.path.join(name, "*.jpg"))
+            files = glob.glob(os.path.join(str(name[1]), "*.jpg"))
             for i in range(len(files)):
                 if i < (1-val_rate)*len(files):
-                    shutil.copy(files[i], f"train/{name}{i}.jpg")
+                    shutil.copy(files[i], f"train/{str(name[0])}/{name[1]}{i}.jpg")
                 else:
-                    shutil.copy(files[i], f"validation/{name}{i}.jpg")
+                    shutil.copy(files[i], f"validation/{str(name[0])}/{name[1]}{i}.jpg")
+            shutil.rmtree(name[1])
 
 
-my_crawler = ImageCrawler(["gorilla", "orangutan", "chimpanzee"])
-my_crawler.crawl(100)
-my_crawler.split(0.2)
+my_crawler1 = ImageCrawler([(0, "gorilla"), (1, "orangutan"), (2, "chimpanzee")])
+my_crawler1.crawl(100)
+my_crawler1.split(0.2)
+my_crawler2 = ImageCrawler([(0, "ゴリラ"), (1, "オラウータン"), (2, "チンパンジー")])
+my_crawler2.crawl(100)
+my_crawler2.split(0.2)
