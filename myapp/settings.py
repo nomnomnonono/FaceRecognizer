4f7479ae05e2a4fdcbedd616cf5e48bda89b8bbd
nomnomnonono <murumuru1204@gmail.com>
2021-08-25 23:46:48 +0900

    set False to APPEND_SLASH

--- myapp/settings.py
+++ myapp/settings.py
@@ -16,6 +16,7 @@ from .setting_secret import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+APPEND_SLASH = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recognizer.apps.BlogConfig',
+    'recognizer.apps.RecognizerConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
