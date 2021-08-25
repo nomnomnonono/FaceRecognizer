696e1e8b72aa50a41608dbc4ec2b75e1e07fd7a9
nomnomnonono <murumuru1204@gmail.com>
2021-08-25 23:51:56 +0900

    add View.

--- recognizer/views.py
+++ recognizer/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+from .forms import UploadImgForm
+from mymodel.recognizer import recognizer
 
-# Create your views here.
+
+def index(request):
+    params = {
+        "form": UploadImgForm()
+    }
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(params, request))
+
+
+def predict(request):
+    if not request.method == 'POST':
+        raise redirect('recognizer:index')
+
+    form = UploadImgForm(request.POST, request.FILES)
+
+    if not form.is_valid():
+        raise ValueError('Form is invalid.')
+
+    img = form.cleaned_data['img']
+    pred, prob = recognizer(img)
+    template = loader.get_template('predict.html')
+    params = {
+        'pred': pred,
+        'prob': prob
+    }
+    return HttpResponse(template.render(params, request))
